b03175f6855d0756bb10741adbf3f6473f3d402e
duluyang <272197280@qq.com>
2019-06-19 16:44:58 +0800

    首次提交

--- DDBaseView/Classes/Controller/FirstViewController.m
+++ DDBaseView/Classes/Controller/FirstViewController.m
@@ -16,6 +16,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"第一页面";
+    self.view.backgroundColor = [UIColor redColor];
     // Do any additional setup after loading the view.
 }
 
